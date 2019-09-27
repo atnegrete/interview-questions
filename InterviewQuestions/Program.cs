c16b8b92640b2cf8ac13225189e85f3752b440ef
Alan <alan.t.negrete@gmail.com>
2019-09-26 19:36:26 -0500

    initial commit

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -1,12 +1,117 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace InterviewQuestions
 {
     class Program
     {
+
+        public static Dictionary<string, string> Dictionary;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Dictionary = new Dictionary<string, string>();
+
+            var path = Directory.GetCurrentDirectory() + "\\Inputs\\words.txt";
+
+            string[] lines = File.ReadAllLines(path);
+
+            List<string> linesDesc = lines
+                .ToList()
+                .Where(line => lines.Contains(line))
+                .OrderByDescending(line => line.Length)
+                .ToList();
+
+            linesDesc.ForEach(w =>
+            {
+                var cur = Dictionary;
+                foreach (char c in w)
+                {
+                    cur.Add(c.ToString(), "");
+                }
+            });
+
+            List<string> winners = new List<string>();
+
+            foreach (string line in linesDesc)
+            {
+                if(IsComposedOfOtherWords(Dictionary, Dictionary, line))
+                {
+                    winners.Add(line);
+                    if(winners.Count() == 2)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            #region Initial Approach
+            //List<string> linesAsc = lines
+            //    .ToList()
+            //    .Where(line => lines.Contains(line))
+            //    .OrderBy(line => line.Length)
+            //    .ToList();
+
+
+            //// Start finding the biggest 2 words (asc -> desc)
+            //// that can be built using other words, starting
+            //// with the smallest words (desc -> asc)
+            //foreach (string desc in linesDesc)
+            //{
+            //    string descLine = desc;
+
+            //    foreach(string asc in linesAsc)
+            //    {
+            //        if(descLine.Length == 0)
+            //        {
+            //            winners.Add(desc);
+            //            break;
+            //        }
+
+            //        int index;
+            //        while((index = descLine.IndexOf(asc)) >= 0)
+            //        {
+            //            // Small line exists in big line
+            //            if (index >= 0)
+            //            {
+            //                descLine = descLine.Remove(index, asc.Length);
+            //            }
+            //        }
+            //    }
+
+            //    if(winners.Count() == 2)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            #endregion
+
+            //winners.ForEach(w => Console.WriteLine(w));
+            Console.ReadKey();
+        }
+
+        public static bool IsComposedOfOtherWords(Dictionary<string, string> baseDict, Dictionary<string, string> curDict, string word, int level = 0)
+        {
+            string first = word[0].ToString();
+            if(!curDict.TryGetValue(first, out string found))
+            {
+                return false;
+            }
+
+            if(word.Length == 1 && found == first)
+            {
+                return level > 0;
+            }
+
+            if (found == first && IsComposedOfOtherWords(baseDict, baseDict, word.Substring(1), level + 1))
+                return true;
+
+
+            return false;
         }
     }
 }
---
+++ InterviewQuestions/Readme.txt
@@ -0,0 +1,9 @@
+﻿- Was not able to find solution in time.
+
+- Initial approach, I first attempted to sort through the list (asc & desc by length). Then loop through them in desc order while removing the smaller words.
+ Then I proceeded to also remove duplicates of the smaller words. However, this approach did not work because I was removing subsets of the original word with a 
+ a smaller word, even though there could be a larder word that would fit.
+ A possible solution would be to add more nesting into the algorithim to make sure to re-check all those as well, but that would of increased big o by a lot.
+
+ - At this point I realized I only had 15 minutes left and attempted to research the problem on stack overflow, but ran out of time to implement my own solution
+ via the Trie method.
